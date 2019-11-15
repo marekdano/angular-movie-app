db44c665d1db445793c1ad0baa5e8bed780988fe
Marek Dano <mk.dano@gmail.com>
2019-11-15 09:21:02 +0000

    Add to display loading state and manipulate a response from web API before display it in the home component.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 import { SharedService } from '~app/shared/shared.service';
-import { Movie } from '~app/shared/shared.interface';
+import { Movie, MovieApiResponse, MovieApi } from '~app/shared/shared.interface';
+import { DEFAULT_PLACEHOLDER_IMAGE } from '~app/shared/utils';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -12,8 +14,8 @@ import { Observable } from 'rxjs';
 export class HomeComponent implements OnInit {
   loading: boolean = false;
   errorMessage: string = '';
-  moviesObservable: Observable<Movie[]>;
-  
+  movies$: Observable<Movie[]> = null;
+
   constructor(
     private sharedService: SharedService, 
   ) { }
@@ -21,6 +23,20 @@ export class HomeComponent implements OnInit {
   ngOnInit() { }
 
   onSubmittedSearchForm(query: string) {
-    this.moviesObservable = this.sharedService.getMoviesByQuery(query);
+    this.movies$ = this.sharedService.getMoviesByQuery(query)
+      .pipe(
+        map((apiResponse: MovieApiResponse) => apiResponse['Response'] === 'True' ? apiResponse['Search'] : []),
+        map((items: MovieApi[]) => (
+          items.map(item => (
+            {
+              id: item.imdbID,
+              title: item.Title,
+              year: item.Year,
+              type: item.Type,
+              poster: item.Poster !== 'N/A' ? item.Poster : DEFAULT_PLACEHOLDER_IMAGE,
+            }
+          )) as Movie[]
+        ))
+      )
   }
 }
