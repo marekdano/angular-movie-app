59449d1a4892fa832cc812bbedff0c3c0624182a
Marek Dano <mk.dano@gmail.com>
2019-11-25 22:55:36 +0000

    Consume searchTermService in the search component for handling searchTerm value.

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SearchTermService } from '~app/store/search-term.service';
 
 @Component({
   selector: 'app-search',
@@ -12,16 +13,26 @@ export class SearchComponent implements OnInit {
   @Output() searchTerm = new EventEmitter<string>()
 
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private searchTermService: SearchTermService,
   ) { }
 
   ngOnInit() {
     this.searchForm = this.fb.group({
       term: ['', Validators.required]
     })
+
+    if (this.searchTermService.searchTerm !== null) {
+      this.searchForm.patchValue({term: this.searchTermService.searchTerm})
+      this.searchTerm.emit(this.searchForm.value.term)
+    }
   }
 
   onSubmit() {
-    this.searchTerm.emit(this.searchForm.value.term);
+    const termValue = this.searchForm.value.term as string
+    if (termValue) {
+      this.searchTermService.searchTerm = termValue
+      this.searchTerm.emit(this.searchForm.value.term)
+    }
   }
 }
